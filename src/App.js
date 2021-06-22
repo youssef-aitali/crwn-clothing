a718d19580829d9e37f13832fe5db0e80ada8577
Youssef AIT ALI <youssef.aitali@outlook.com>
2021-06-22 22:44:48 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HompePage from './pages/homepage/homepage.component';
 
+const HomePage = () => (
+  <div>
+    <h1>HOME PAGE</h1>
+  </div>
+);
+
+const TopicsList = () => (
+  <div>
+    <h1>TOPIC LIST PAGE</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HompePage />
+      <Route exact path='/' component={HompePage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
     </div>
   );
 }
